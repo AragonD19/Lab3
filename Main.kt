40128ee773cf2376c8aeef8da1e23eba216ecfb8
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-07-22 08:12:46 -0600

    Mi programa

---
+++ Main.kt
@@ -0,0 +1,78 @@
+class Main {
+    /**
+     * You can edit, run, and share this code.
+     * play.kotlinlang.org
+     */
+    data class ItemData(
+        var originalPos: Int,
+        var originalValue: Any?,
+        var type: String? = null,
+        var info: String? = null
+    )
+// -----------------------
+
+    fun main() {
+        val result = processList(listOf(25, "Hola", null, false, 2.0))
+        println(result)
+    }
+
+    fun processList(inputList: List<Any?>?): List<ItemData>? {
+        var resultado = ArrayList<ItemData>()
+        var cont: Int = -1
+        var type: String? = null
+        var info: String? = null
+
+        if (inputList != null){
+            for (item in inputList){
+                cont = cont + 1
+                when (item) {
+                    is String -> {
+                        type = "String"
+                        var a = item.length
+                        info = "L"+ a
+                        resultado.add(ItemData(cont,(item ?: null),type,info))
+                    }
+                    is Int -> {
+                        type = "Int"
+                        var a = 0
+                        if (item %10 == 0){
+                            a = 10
+                        }
+                        if (item %5 == 0){
+                            a = 5
+                        }
+                        if (item %2 == 0){
+                            a = 2
+                        }
+                        info = "M"+ a
+                        resultado.add(ItemData(cont,(item ?: null),type,info))
+                    }
+                    is Boolean -> {
+                        type = "Boolean"
+                        if (item) {
+                            info = "verdadero"
+                            resultado.add(ItemData(cont,(item ?: null),type,info))
+                        }
+                        else {
+                            info = "false"
+                            resultado.add(ItemData(cont,(item ?: null),type,info))
+                        }
+
+                    }
+
+                    else -> {
+                        type = null
+                        info = null
+                        resultado.add(ItemData(cont,(item ?: null),type,info))
+                    }
+                }
+
+            }
+        }
+
+
+
+        return resultado
+
+    }
+}
